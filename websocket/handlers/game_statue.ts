d57fcb50c451e26c03ba8bb817c53c637f8ce8e1
Pikacnu <a13456123456@gmail.com>
2025-07-08 20:41:09 +0800

    feat: implement game status handler with storage and map data processing

--- websocket/handlers/game_statue.ts
+++ websocket/handlers/game_statue.ts
@@ -24,51 +24,26 @@ export const handler: Handler = async ({ ws, message, client, logger }) => {
 
 	try {
 		const statusData = payload.data as {
-			status: GameStatus;
+			storage: string;
+			key: string;
+			data: string[];
 		};
-		const gameStatus = (statusData as { status: GameStatus }).status;
+
+		const data = JSON.parse(MinecraftNbtProcessToJson(statusData.data[0]));
+		const banCharacter: number[] = data?.ban;
+		const map: number = data?.map;
 		if (client?.game?.id) {
 			await db
 				.update(gameTable)
 				.set({
-					status: gameStatus,
+					banCharacter: banCharacter,
+					mapId: map,
+					status: GameStatus.idle,
 				})
 				.where(eq(gameTable.id, client.game.id))
 				.execute();
-		} else {
-			throw new WebSocketError('No game to update status');
 		}
-		if (gameStatus === GameStatus.idle) {
-			if (client?.game) {
-				//send player to lobby
-				ws.send(
-					JSON.stringify({
-						status: status.success,
-						action: Action.transfer,
-						payload: {
-							transferData: {
-								targetServer: 'ranked-uta.pikacnu.com',
-								targetPort: 25565,
-								uuids: client.game.players.map((p) => p.uuid),
-							},
-						},
-					}),
-				);
-				client.game = undefined;
-				client.status = ServerStatus.pending;
-				ws.send(
-					JSON.stringify({
-						status: status.success,
-						action: Action.whitelist_change,
-						payload: {
-							whitelist: [],
-						},
-					}),
-				);
-			} else {
-				throw new WebSocketError('No game to set to idle');
-			}
-		}
+		return;
 	} catch (error) {
 		logger.error('Invalid game status', error);
 		throw new WebSocketError('Invalid game status');
